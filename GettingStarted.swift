305766257b29a9f38b776197e938687ad86f3e18
prashanth-g <prashanthcsgce@gmail.com>
2018-12-11 14:17:33 +0530

    Initial Commit

---
+++ GettingStarted.swift
@@ -0,0 +1,12 @@
+// variables
+/*
+Constant and variable names can’t contain whitespace characters, mathematical symbols, arrows, private-use Unicode scalar values, or line- and box-drawing characters. Nor can they begin with a number, although numbers may be included elsewhere within the name.
+*/
+let maxLoginAttemptsAllowed = 10
+var currentLoginAttempts = 0
+
+let π = 3.14159
+let 你好 = "你好世界"
+let 🐱🐶 = "cat and dog"
+
+print(🐱🐶)
